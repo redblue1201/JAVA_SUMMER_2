bbda83f46bb156129f1b0f5d1dd8bad36cda0a72
nightfury <redblue1201@kyungmin.ac.kr>
2023-06-30 13:52:00 +0900

    JAVA_2

---
+++ src/com/java/ex/MainClass.java
@@ -0,0 +1,25 @@
+package com.java.ex;
+
+import java.sql.SQLOutput;
+
+public class MainClass {
+    public static void main(String[] args) {
+        boolean boolD = true;
+        char charD = 'A';
+        byte byteD = 127;
+        short shortD = 10000;
+        int intD = 100000000;
+        long longD = 1000000000;
+        float floatD = 3.14F;
+        double doubleD = 3.14D;
+
+        System.out.println(boolD);
+        System.out.println(charD);
+        System.out.println(byteD);
+        System.out.println(shortD);
+        System.out.println(intD);
+        System.out.println(longD);
+        System.out.println(floatD);
+        System.out.println(doubleD);
+    }
+}
